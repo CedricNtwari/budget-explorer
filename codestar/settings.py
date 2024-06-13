6322b63a4121760aefed52baed5990f6ee290a7c
CedricNtwari <cedric.ntwari@powercoders.org>
2024-06-13 19:39:25 +0200

    Code for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$qto1-5guvys0obmr1ck6hzcu_e!mfiwqgfx9i%#%q_wlsk*-(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
