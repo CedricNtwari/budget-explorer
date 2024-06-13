68bdac19ef97cd95fc9cbef2a2efa02ab5c09896
CedricNtwari <cedric.ntwari@powercoders.org>
2024-06-13 20:32:53 +0200

    Remove the import of env module from settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-# Load environment variables from env.py
-from env import *
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
