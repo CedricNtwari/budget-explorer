0db13167ac3fa1474ac5185514fbe1dd1fc6e831
CedricNtwari <cedric.ntwari@powercoders.org>
2024-06-30 23:31:46 +0200

    Added `comment.js` to handle comment editing functionality

--- discover/views.py
+++ discover/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.conf import settings
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import JsonResponse
 from django.template.loader import render_to_string
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from django.contrib import messages
 from .forms import CommentForm
 
@@ -76,3 +77,27 @@ def post_detail(request, slug):
         {"post": post, "comments": comments,
             "comment_count": comment_count, "comment_form": comment_form},
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
